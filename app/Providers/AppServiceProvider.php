ca81b394cc9e1cc339593059d551ea1a2e199257
rutuldev1985 <rutuldev1985@gmail.com>
2025-04-06 16:32:08 +0530

    setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Repositories\Admin\Product\ProductInterface;
+use App\Repositories\Admin\Product\ProductRepository;
+use App\Repositories\Auth\Login\LoginInterface;
+use App\Repositories\Auth\Login\LoginRepository;
+use App\Repositories\Auth\Register\RegisterInterface;
+use App\Repositories\Auth\Register\RegisterRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +17,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(LoginInterface::class, LoginRepository::class);
+        $this->app->bind(RegisterInterface::class, RegisterRepository::class);
+        $this->app->bind(ProductInterface::class, ProductRepository::class);
     }
 
     /**
